c15e26041d4e2dbd65f9dc05868ce86751d3729c
fidias-i <fidias-i@users.noreply.github.com>
2024-01-30 19:11:14 +0000

    ✅ Ready to clone and code.

--- project_health/cli.py
+++ project_health/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for project_health project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m project_health` and `$ project_health `.
 
     This is your program's entry point.
 
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for project_health package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("project_health", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="project_health",
+    version=read("project_health", "VERSION"),
+    description="Awesome project_health created by fidias-i",
+    url="https://github.com/fidias-i/Project-Health/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="fidias-i",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["project_health = project_health.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
